e00ab9d3304a351037b6e84b946863ae6a83a9ed
Kenny Zsombik <kenny.zsombik@capgemini.com>
2019-05-12 20:10:56 +0200

    setup gemaakt voor RA

--- src/test/java/rest/examples/GetRequestExample.java
+++ src/test/java/rest/examples/GetRequestExample.java
@@ -1,4 +1,34 @@
 package rest.examples;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+
+
 public class GetRequestExample {
+
+
+    @BeforeEach
+    void setUp()
+    {
+        RestAssured.baseURI = "http://services.groupkt.com";
+        RestAssured.port = 80;
+    }
+
+    @Test
+    @DisplayName("Testing that my Get Request has ")
+    public void simpleGetRequestWithStatusCode()
+    {
+        when().get("/country/get/all").then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void simpleGetRequestWithBodyValidation()
+    {
+
+    }
 }
