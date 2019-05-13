5553f873323080f497d38b1c1978a0eab401544d
Kenny Zsombik <kenny.zsombik@capgemini.com>
2019-05-13 16:51:06 +0200

    added json + example request

--- src/test/java/rest/examples/GetRequestExample.java
+++ src/test/java/rest/examples/GetRequestExample.java
@@ -1,11 +1,18 @@
 package rest.examples;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.concurrent.TimeUnit;
+
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.lessThan;
+
 
 
 public class GetRequestExample {
@@ -29,6 +36,17 @@ public class GetRequestExample {
     @Test
     public void simpleGetRequestWithBodyValidation()
     {
+        when().get("/country/get/all")
+                .then().body("messages",contains("Total"));
+
+    }
+
+    @Test
+    @DisplayName("Validating that a request time is less than 20s")
+    public void performanceMeasureOfGetRequest()
+    {
+        when().get("/country/get/all")
+        .then().time(lessThan(20L),TimeUnit.SECONDS);
 
     }
 }
